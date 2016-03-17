98020601b6a5bf3d24a6c29608182fc4d0cf9832
Kiran Nagaraj <kirankn@gmail.com>
2016-03-17 15:45:18 +0530

    shdklhakljshd

--- inimage.js
+++ inimage.js
@@ -7,20 +7,20 @@
 */
 (function() {
 	var $isMobile = !!((/android|webos|iphone|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()))),
-		//$onceCalled = false,
+		$onceCalled = false,
 		jqueryLink = '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
 		debugRequired = true,
 		$,
 		_window_dataObject = {};
 
-  if(window.hoverrneo === undefined) window.hoverrneo = {};
+  if(window._pm_object === undefined) window._pm_object = {};
 
-	// var Debugger = function () { };
-	// Debugger.log = function (message) {
-	// 	try {
-	// 		if(debugRequired) console.log(message);
-	// 	} catch (exception) {}
-	// };
+	var Debugger = function () { };
+	Debugger.log = function (message) {
+		try {
+			if(debugRequired) console.log(message);
+		} catch (exception) {}
+	};
 
 	var findContainer = function(callback){
 		var tempObject = [];
@@ -64,17 +64,19 @@
 		$cssText,
 		$top,
 		$adTag = _window_dataObject.tag,
-		$key = [window.hoverrneo.host, window.hoverrneo.section, window.hoverrneo.web_or_mobile, _window_dataObject.id, _window_dataObject.is_default],
+		$key = [window._pm_object.host, window._pm_object.section, window._pm_object.web_or_mobile, _window_dataObject.id, _window_dataObject.is_default],
 		$pixelPingHtml = '<img src="http://track.hoverr.media/pixel.gif?key='+ $key.join('|') +'" style="display:none;"/>';
 
 		$cssText = "height:"+ $iframeHeight +"px !important; min-height:"+ $iframeHeight +"px !important; width:"+ $iframeWidth +"px !important; overflow:hidden;"+ $left +"bottom:"+ $bottom +"px;margin:0 auto; position:absolute; background: transparent; z-index: 2; -moz-transform: scale("+ $scale +"); -o-transform: scale("+ $scale +"); -webkit-transform: scale("+ $scale +"); transform: scale("+ $scale +"); max-width:initial !important;";
 
 		setAttributes(iframe, {"height": $iframeHeight, "scrolling": "no", "frameBorder": 0, "allowtransparency": "true", "class": "_adSenceImagePush", "width": $iframeWidth, "loadStatus": 0, "data-width": $iframeWidth, "data-height": $iframeHeight, "style":$cssText});
 
+    console.log(iframe);
+
 		container.containerDiv.append(iframe);
 
 		try{
-			console.log('Log : Setting html content in try block !!');
+			Debugger.log('Log : Setting html content in try block !!');
 			var doc = iframe.contentDocument || iframe.contentWindow.document;
 			doc.open();
 			doc.write('<head><style>html{margin:0 !important;background:transparent;}body{margin:0 !important;position:relative !important;border:0;background-color:transparent;}img{width:100% !important;}object{width:100% !important;}body iframe{width:100% !important;background:transparent;}</style><script type="text/javascript">var d=document,w=window,interval,el=w.frameElement,mIn,vIn;function init(){interval=setInterval(function(){if(d.getElementsByTagName("iframe").length>0||d.getElementsByTagName("img").length>0||d.getElementsByTagName("object").length>0||d.getElementsByTagName("embed").length>0){w.clearInterval(interval);var ip=d.getElementsByTagName("iframe");if(ip.length==0){ani();}else{ip[0].onload=ani();}}},200);}function ani(){ el.setAttribute("loadStatus",1); d.body.style.display="block"; } function bdn(){el.style.display="none";} \x3C/script></head><body onload="init()"> <div style="position: absolute;top: 0;height: 18px;width: 100%;"><!-- <a href="http://www.hoverr.me" target="_blank" style="float: right;margin-right: 25px;color: #ffffff;text-shadow: 1px 1px 2px #000000;font-family: monospace;line-height: 18px;font-size: 14px;"">Advertisement</a> --><span id="adSenceImagePushClose" style="position:absolute;top:0;right:0;height:19px;width:19px;text-align:center;cursor:pointer;font-size:19px;line-height:18px;color:#F0F0F0;z-index:2147483648;background:#C1C1C1;" onClick="bdn()" title="Close">&times</span></div>'+ $adTag + $pixelPingHtml +'</body>');
@@ -85,27 +87,41 @@
 
 	// After deciding which jQuery to use this function get called
 	var initialize = function(){
-		console.log('Log : Initialize function called !!');
+		Debugger.log('Log : Initialize function called !!');
 		// if ($isMobile) return;
 
-    console.log($onceCalled);
-
-		// if(!$onceCalled) {
-		// 	$onceCalled = true;
+		//if(!$onceCalled) {
+		//	$onceCalled = true;
 			$ = $ || window.jQuery || window.$;
+	
+			// console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+			// //console.log(hoverrneo);
+   //    //neo = JSON.parse(tmpneo);
 
-      console.log(window.hoverrneo);
-      console.log(window);
+			// console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+			
+			if(window.hoverrneo != undefined) {
+			  console.log(hoverrneo);
+			  _window_dataObject = hoverrneo;
+        window._pm_object.host = hoverrneo.h;
+        window._pm_object.section = hoverrneo.s;
+        window._pm_object.web_or_mobile = hoverrneo.wm;
+      } else if(window._pm_object === undefined || window._pm_object.inimage == undefined) {
+        return;
+      } else {
+        _window_dataObject = window._pm_object.inimage;
+			}
 
-			if(window.hoverrneo === undefined || window.hoverrneo.inimage == undefined) return;
-			else _window_dataObject = window.hoverrneo.inimage;
+      console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+      console.log(_window_dataObject);
+      console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
 
 			findContainer(function(container){
 				$.each(container, function(i, v){
 					putAd(container[i]);
 				});
 			});
-	//	}
+		//}
 	};
 
 	// Create script tag with forwarded link and put under the head section and get back to scriptLoadHandler function
