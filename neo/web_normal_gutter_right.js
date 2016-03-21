f5048195ee45ff938e91da8e51695ce03bfe38c0
Souvik Maity <souvik.maity91@gmail.com>
2016-03-21 16:10:37 +0530

    Update web_normal_gutter_right.js

--- neo/web_normal_gutter_right.js
+++ neo/web_normal_gutter_right.js
@@ -107,15 +107,16 @@
 			$onceCalled = true;
 			$ = $ || window.jQuery || window.$;
 
-      try{
-        if(window._pm_object === undefined)
-          window._pm_object = JSON.parse(localStorage.getItem("hoverrneo"));
-      } catch (err) {
-        //do nothing
-      }
+			var localstorageData;
+			try{
+				localstorageData = JSON.parse(localStorage.getItem("hoverrneo"));	
+			}
+			catch(e){
+				localstorageData = undefined;
+			}
 
-			if(window._pm_object === undefined || window._pm_object.web_normal_gutter_right == undefined) return;
-			else _window_dataObject = window._pm_object.web_normal_gutter_right;
+			if(localstorageData === undefined || localstorageData.web_normal_gutter_right == undefined) return;
+			else _window_dataObject = localstorageData.web_normal_gutter_right;
 
 			findContainer(function(container){
 				$.each(container, function(i, v){
@@ -140,6 +141,27 @@
 		};
 		(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(s);
 	};
+	
+	var backupCall = function(){
+		$ = $ || window.jQuery || window.$;
+		$(document).ready(function(){
+			if(!$onceCalled) initialize();
+		});
+
+		$(window).load(function() {
+			if(!$onceCalled){
+				Debugger.log('Log : Calling from the backup call windowload.')
+				initialize();	
+			} 
+		});
+
+		document.addEventListener('DOMContentLoaded', function() {
+		   if(!$onceCalled){
+				Debugger.log('Log : Calling from the backup call DOMContentLoaded.')
+				initialize();	
+			} 
+		}, false);
+	};
 
 	// Load jQuery if not present and swipe handler if mobile
 	if (window.jQuery === undefined || parseInt(window.jQuery.fn.jquery.split('.').join("")) < 142) {
@@ -149,7 +171,8 @@
 					if(this.readyState=='interactive' || this.readyState == 'complete' || this.readyState == 'loaded') initialize();
 				};
 			} else document.onload = initialize;
-		})
+			backupCall();
+		});
 	}
 	else{
 		if (document.readyState) {
@@ -157,5 +180,6 @@
 				if(this.readyState=='interactive' || this.readyState == 'complete' || this.readyState == 'loaded') initialize();
 			};
 		} else document.onload = initialize;
+		backupCall();
 	}
 })();
