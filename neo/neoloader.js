a900421d15bd9dea61ad6e7432c45aee1d395668
kirankn <kirankn@gmail.com>
2016-03-23 01:59:16 +0530

    Update neoloader.js
    kkn

--- neo/neoloader.js
+++ neo/neoloader.js
@@ -1,6 +1,8 @@
-try {
-  if (window._pm_object === undefined)
-    window._pm_object = JSON.parse(localStorage.getItem("hoverrneo"));
-} catch (err) {
-  //do nothing
-}
+(function() {
+  try {
+    if (window._pm_object === undefined)
+      window._pm_object = JSON.parse(localStorage.getItem("hoverrneo"));
+  } catch (err) {
+    //do nothing
+  }
+})();
