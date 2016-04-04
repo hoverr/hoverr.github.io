24afb0d83b73c1847913b6e5802c6faeb9e0fdec
Kiran Nagaraj <kirankn@gmail.com>
2016-04-04 12:06:01 +0530

    VAST test

---
+++ simple/ads.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var player = videojs('content_video');
+
+var options = {
+  id: 'content_video',
+  adTagUrl: 'https://googleads.g.doubleclick.net/pagead/ads?client=ca-video-pub-4631387039185633&slotname=5984306776&ad_type=standardvideo&description_url=http%3A%2F%2Fintoday.in&videoad_start_delay=0&type=all'
+};
+
+player.ima(options);
+
+// Remove controls from the player on iPad to stop native controls from stealing
+// our click
+var contentPlayer =  document.getElementById('content_video_html5_api');
+if ((navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/Android/i)) &&
+    contentPlayer.hasAttribute('controls')) {
+  contentPlayer.removeAttribute('controls');
+}
+
+// Initialize the ad container when the video player is clicked, but only the
+// first time it's clicked.
+var startEvent = 'click';
+if (navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/Android/i)) {
+  startEvent = 'tap';
+}
+
+player.one(startEvent, function() {
+    player.ima.initializeAdDisplayContainer();
+    player.ima.requestAds();
+    player.play();
+});
