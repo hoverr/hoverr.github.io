a535189b3f0f51544f53c4d36ce73de5b3d1c476
kirankn <kirankn@gmail.com>
2016-04-13 16:24:42 +0530

    Update ads.js

--- simple/ads.js
+++ simple/ads.js
@@ -29,6 +29,8 @@ var options = {
   //adTagUrl: 'https://ima3vpaid.appspot.com/?adTagUrl=https%3A%2F%2Fgoogleads.g.doubleclick.net%2Fpagead%2Fads%3Fclient%3Dca-video-pub-4631387039185633%26slotname%3D3539573776%26ad_type%3Dvideo%26description_url%3Dhttp%253A%252F%252Fwebdunia.com%26videoad_start_delay%3D0&type=all';
   adTagUrl: 'https://googleads.g.doubleclick.net/pagead/ads?client=ca-video-pub-4631387039185633&slotname=3539573776&ad_type=video&description_url=http%3A%2F%2Fwebdunia.com&videoad_start_delay=0&type=all';
 
+   //Working demo tag
+  //adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
 };
 
 player.ima(options);
