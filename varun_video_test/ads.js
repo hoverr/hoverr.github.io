8d0d7a1334d00a1567b10d781694885a300af53c
hoverr <accounts@hoverr.co>
2016-04-13 17:19:07 +0530

    Create ads.js

---
+++ varun_video_test/ads.js
@@ -0,0 +1,135 @@
+/**
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Ads = function() {
+
+  this.player = videojs('content_video');
+
+  this.adTagInput = document.getElementById('tagInput');
+  this.sampleAdTag = document.getElementById('sampleAdTag');
+  this.sampleAdTag.addEventListener(
+      'click',
+      this.bind(this, this.onSampleAdTagClick_),
+      false);
+
+  // Remove controls from the player on iPad to stop native controls from stealing
+  // our click
+  var contentPlayer =  document.getElementById('content_video_html5_api');
+  if ((navigator.userAgent.match(/iPad/i) ||
+          navigator.userAgent.match(/Android/i)) &&
+      contentPlayer.hasAttribute('controls')) {
+    contentPlayer.removeAttribute('controls');
+  }
+
+  // Start ads when the video player is clicked, but only the first time it's
+  // clicked.
+  var startEvent = 'click';
+  if (navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/Android/i)) {
+    startEvent = 'tap';
+  }
+  this.player.one(startEvent, this.bind(this, this.init));
+
+  this.options = {
+    id: 'content_video'
+  };
+
+  this.events = [
+    google.ima.AdEvent.Type.ALL_ADS_COMPLETED,
+    google.ima.AdEvent.Type.CLICK,
+    google.ima.AdEvent.Type.COMPLETE,
+    google.ima.AdEvent.Type.FIRST_QUARTILE,
+    google.ima.AdEvent.Type.LOADED,
+    google.ima.AdEvent.Type.MIDPOINT,
+    google.ima.AdEvent.Type.PAUSED,
+    google.ima.AdEvent.Type.STARTED,
+    google.ima.AdEvent.Type.THIRD_QUARTILE
+  ];
+
+  this.console = document.getElementById('ima-sample-console');
+  this.player.ima(
+      this.options,
+      this.bind(this, this.adsManagerLoadedCallback));
+
+};
+
+Ads.prototype.SAMPLE_AD_TAG = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=';
+
+/*
+	adTagUrl : 'http://pubads.g.doubleclick.net/gampad/ads?' +
+    'sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&' +
+    'ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&' +
+    'unviewed_position_start=1&' +
+    'cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&' +
+    'vid=short_onecue&correlator=';
+
+  adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
+      'iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&' +
+      'impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&' +
+      'cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&' +
+      'vid=short_onecue&correlator='
+
+
+  adTagUrl:'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external'+
+            '/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed'+
+            '_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator='
+
+
+  adTagUrl:'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/175265624/'+
+			'vast_test&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start'+
+			'=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]'
+  adTagUrl: 'https://googleads.g.doubleclick.net/pagead/ads?client=ca-video'+
+             '-pub-4631387039185633&slotname=3539573776&ad_type=video&description_url'+
+             '=http%3A%2F%2Fwebdunia.com&videoad_start_delay=0&type=all'
+*/
+Ads.prototype.init = function() {
+  if (this.adTagInput.value == '') {
+    this.log('Error: please fill in an ad tag');
+  } else {
+    this.player.ima.initializeAdDisplayContainer();
+    this.player.ima.setContent(null, this.adTagInput.value, true);
+    this.player.ima.requestAds();
+    this.player.play();
+  }
+};
+
+Ads.prototype.onSampleAdTagClick_ = function() {
+  this.adTagInput.value = this.SAMPLE_AD_TAG;
+};
+
+Ads.prototype.adsManagerLoadedCallback = function() {
+  for (var index = 0; index < this.events.length; index++) {
+    this.player.ima.addEventListener(
+        this.events[index],
+        this.bind(this, this.onAdEvent));
+  }
+  this.player.ima.start();
+};
+
+Ads.prototype.onAdEvent = function(event) {
+  this.log('Ad event: ' + event.type);
+};
+
+Ads.prototype.log = function(message) {
+  this.console.innerHTML = this.console.innerHTML + '<br/>' + message;
+}
+
+Ads.prototype.bind = function(thisObj, fn) {
+  return function() {
+    fn.apply(thisObj, arguments);
+  };
+};
