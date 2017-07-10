6aec78d45f5d3100992446a160104d985ad499f1
Corefinder89 <soumyajit.basu62@gmail.com>
2017-07-10 13:22:29 +0530

    Modify javascript code

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,10 @@
-var hoverr = window.setInterval(function(){
-  var checkAdLoad = function(){
-    if($("._teraAdContainer")){
-      console.log("Ad unit is present")
-    }
-    else{
-      console.log("Ad unit is not present")
-    }
-  };
-  var validateAd = new CustomEvent("trackAdLoad", { "detail": checkAdLoad });
-  window.dispatchEvent(validateAd);
-},3000);
+var checkAdLoad = function(){
+  if($("._teraAdContainer")){
+    console.log("Ad unit is present")
+  }
+  else{
+    console.log("Ad unit is not present")
+  }
+};
+var validateAd = new CustomEvent("trackAdLoad", { "detail": checkAdLoad });
+window.dispatchEvent(validateAd);
