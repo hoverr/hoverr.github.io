bf0b2045766b05f3a90fed44e3231a2c651bef42
Corefinder89 <soumyajit.basu62@gmail.com>
2017-07-10 15:32:30 +0530

    Changes in the js script

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,20 @@
-window.checkAdLoad = function(){
+// window.checkAdLoad = function(){
+//   var checkAdLoadfunction = function(){
+//     var msg = "Ad unit is not present";
+//     if($("._teraAdContainer")){
+//       console.log("Ad unit is present")
+//       msg = "Ad unit is present";
+//     }
+//     return msg;
+//   }
+//   checkAdLoadfunction()
+//   console.log(checkAdLoadfunction);
+//   var validateAd = new CustomEvent("trackAdLoad", { "detail": checkAdLoadfunction});
+//   window.dispatchEvent(validateAd);
+// }
+
+$(document).ready(function(){
+  console.log("Checking the AdInstances.");
   var checkAdLoadfunction = function(){
     var msg = "Ad unit is not present";
     if($("._teraAdContainer")){
@@ -7,10 +23,5 @@ window.checkAdLoad = function(){
     }
     return msg;
   }
-  checkAdLoadfunction()
-  console.log(checkAdLoadfunction);
-  var validateAd = new CustomEvent("trackAdLoad", { "detail": checkAdLoadfunction});
-  window.dispatchEvent(validateAd);
-}
-
-window.checkAdLoad();
+  checkAdLoadfunction();
+});
