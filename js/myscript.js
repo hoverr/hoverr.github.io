d4277706931676cdab19042024241be3b7133a59
Corefinder89 <soumyajit.basu62@gmail.com>
2017-07-10 15:17:51 +0530

    Adding in changes for js

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,11 @@
 window.checkAdLoad = function(){
   var checkAdLoadfunction = function(){
+    var msg = "Ad unit is not present";
     if($("._teraAdContainer")){
       console.log("Ad unit is present")
+      msg = "Ad unit is present";
     }
-    else{
-      console.log("Ad unit is not present")
-    }
+    return msg;
   }
   var validateAd = new CustomEvent("trackAdLoad", { "detail": checkAdLoadfunction });
   window.dispatchEvent(validateAd);
