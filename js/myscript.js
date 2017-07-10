0b79ce4530ac5b3a98883e06ba1bf0e2434aa598
Corefinder89 <soumyajit.basu62@gmail.com>
2017-07-10 16:29:17 +0530

    Add changes to the js file

--- js/myscript.js
+++ js/myscript.js
@@ -16,11 +16,11 @@
 t$(document).ready(function(){
   console.log("Checking the AdInstances.");
   var checkAdLoadfunction = function(){
-    if(t$("._teraAdContainer")){
+    if(t$("._abmMainAdContainer")){
       console.log("Ad unit is present");
       t$('._teraAdContainer').mouseover(function(){
-        t$(this).focus();
-        console.log('Hoverred mouseover tera ad unit')
+        t$(this).find('._abmAdContainer').text("Mouse over tera ad unit")
+        console.log('Hoverred mouse over tera ad unit')
       });
     }
   }
