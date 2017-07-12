c041cd8cb1afdfce7bd232b0e007860a3c66716a
Corefinder89 <soumyajit.basu62@gmail.com>
2017-07-12 18:11:18 +0530

    JS change

--- js/myscript.js
+++ js/myscript.js
@@ -1,44 +1,16 @@
-// window.checkAdLoad = function(){
-//   var checkAdLoadfunction = function(){
-//     var msg = "Ad unit is not present";
-//     if($("._teraAdContainer")){
-//       console.log("Ad unit is present")
-//       msg = "Ad unit is present";
-//     }
-//     return msg;
-//   }
-//   checkAdLoadfunction()
-//   console.log(checkAdLoadfunction);
-//   var validateAd = new CustomEvent("trackAdLoad", { "detail": checkAdLoadfunction});
-//   window.dispatchEvent(validateAd);
-// }
-
 function checkAdLoad(){
   if(t$("._abmMainAdContainer")){
+    return "Ad Instance Present")
     var l = document.querySelectorAll('._abmMainAdContainer').length;
     return l
   } else {
+    return "Ad Instance not present"
     return 0;
   }
 }
 
 setInterval(function(){
   var adLoaded = checkAdLoad();
-  // console.log(adLoaded)
-  // console.log("HE")
   var validateAd = new CustomEvent("adLoaded", { "info": adLoaded,"detail":{"adCount":adLoaded}});
   window.dispatchEvent(validateAd);
 }, 3000);
-
-// t$(document).ready(function(){
-//   console.log("Checking the AdInstances.");
-//   window.checkAdLoadfunction = function(){
-//     if(t$("._abmMainAdContainer")){
-//       console.log("Ad unit is present");
-//       t$('div').mouseover(function(){
-//         t$(this).find('._abmAdContainer')
-//         console.log('Hoverred mouse over tera ad unit')
-//       });
-//     }
-//   }
-// });
