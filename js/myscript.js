0ff864d9a4c9f87c44b5ab932dea2d32d8b0c917
jpmohan1111 <jpmohan1111@gmail.com>
2017-09-04 18:44:48 +0530

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,16 @@
-function checkAdLoad(){
-  if(t$("._abmMainAdContainer")){
-    return "Ad Instance Present")
-    var l = document.querySelectorAll('._abmMainAdContainer').length;
-    return l
-  } else {
-    return "Ad Instance not present"
-    return 0;
-  }
-}
+// function checkAdLoad(){
+//   if(t$("._abmMainAdContainer")){
+//     return "Ad Instance Present")
+//     var l = document.querySelectorAll('._abmMainAdContainer').length;
+//     return l
+//   } else {
+//     return "Ad Instance not present"
+//     return 0;
+//   }
+// }
 
-setInterval(function(){
-  var adLoaded = checkAdLoad();
-  var validateAd = new CustomEvent("adLoaded", { "info": adLoaded,"detail":{"adCount":adLoaded}});
-  window.dispatchEvent(validateAd);
-}, 3000);
+// setInterval(function(){
+//   var adLoaded = checkAdLoad();
+//   var validateAd = new CustomEvent("adLoaded", { "info": adLoaded,"detail":{"adCount":adLoaded}});
+//   window.dispatchEvent(validateAd);
+// }, 3000);
